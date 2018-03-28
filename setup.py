2f2eaa21cde4140806d76120bc00579ccb31a6d7
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-03-28 12:32:51 +0900

    version up

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_lang_data():
 
 setup(
     name="sumeval",
-    version="0.1.5",
+    version="0.1.6",
     description="Well tested evaluation framework for Text summarization",
     url="https://github.com/chakki-works/sumeval",
     author="icoxfog417",
